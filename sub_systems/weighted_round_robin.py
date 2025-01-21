bc7224199f687b81f38a5929a3600d0afadc4f1f
mhkapr <mhkaprogrammer@gmail.com>
2025-01-21 14:56:46 +0330

    add restrict starvation

--- sub_systems/weighted_round_robin.py
+++ sub_systems/weighted_round_robin.py
@@ -5,7 +5,7 @@ from task import SubSystem1Task
 
 
 class WeightedRoundRobinScheduler:
-    def __init__(self, waiting_queue , r1_available, r2_available):
+    def __init__(self, waiting_queue , r1_available, r2_available, lock = None):
         self.queues: list[Queue] = []
         self.weights: list[int] = []
         self.current_index = 0
@@ -14,7 +14,7 @@ class WeightedRoundRobinScheduler:
         self.r1_available = r1_available
         self.r2_available = r2_available
 
-        self._lock = threading.Lock()
+        self._lock = threading.Lock() if lock is None else lock
 
     def add_queue(self, queue, weight):
         with self._lock:
@@ -41,6 +41,34 @@ class WeightedRoundRobinScheduler:
                         if self.add_to_waiting_queue(task):
                             continue
                         return task
+
+
+    def _check_waiting_queue(self):
+        with self._lock:
+            still_waiting = []
+
+            while not self.waiting_queue.empty():
+                task: SubSystem1Task = self.waiting_queue.get()
+                if task.r1_need <= self.r1_available and task.r2_need <= self.r2_available:
+                    self._add_to_ready_queue(task)
+                else:
+                    still_waiting.append(task)
+
+            # Put back tasks that are still waiting
+            for task in still_waiting:
+                self.waiting_queue.put(task)
+
+    def _add_to_ready_queue(self, task: SubSystem1Task):
+        with self._lock:
+            min_queue_len = self.queues[0].qsize()
+            min_sized_queue = self.queues[0]
+            for queue in self.queues:
+                if queue.qsize() < min_queue_len:
+                    min_sized_queue = queue
+                    min_queue_len = queue.qsize()
+            min_sized_queue.put(task)
+
+
 
     def _set_current_index(self):
         self.current_index = (self.current_index + 1) % len(self.queues)
