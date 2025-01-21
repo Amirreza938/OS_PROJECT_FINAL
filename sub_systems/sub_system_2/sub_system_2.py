e64abced932f191916b2d3480aa687aa7ca14622
Amirreza938 <120088072+Amirreza938@users.noreply.github.com>
2025-01-21 16:53:35 +0330

    subsystem 2 and subsystem 3 handled

--- sub_systems/sub_system_2/sub_system_2.py
+++ sub_systems/sub_system_2/sub_system_2.py
@@ -1,25 +1,27 @@
 import threading
 from threading import Thread
-import queue
 from queue import Queue
 from core import BaseCore
 from shortest_remaining_job_first import ShortestRemainingJobFirstScheduler
 from task import BaseTask
+from resource_manager import ResourceManager
 
 class SubSystem2Core(BaseCore):
-    def __init__(self, scheduler:ShortestRemainingJobFirstScheduler, core_id):
-        super().__init__(scheduler, core_id, Queue(), None)
+    def __init__(self, scheduler: ShortestRemainingJobFirstScheduler, core_id, ready_queue):
+        super().__init__(scheduler, core_id, ready_queue, None)
         self.lock = threading.Lock()
         self.scheduler = scheduler
         self.core_id = core_id
         self.clock_event = threading.Event()
-    def run_task(self, task:BaseTask):
+
+    def run_task(self, task: BaseTask):
         task_status = task.execute()
         if task_status == 0:
-            print(f"Task {task.name} completed on core {self.core_id}")  
+            print(f"Task {task.name} completed on core {self.core_id}")
             self.scheduler.release_resource(task)
         elif task_status == -1:
             print(f"Task {task.name} failed on core {self.core_id}")
+
     def run(self):
         while True:
             self.clock_event.wait()
@@ -30,34 +32,50 @@ class SubSystem2Core(BaseCore):
                 if task:
                     self.run_task(task)
                 self.clock_event.clear()
-    def set_clock_event(self):  
+
+    def set_clock_event(self):
         with self.lock:
             self.clock_event.set()
-    def stop(self): 
+
+    def stop(self):
         with self.lock:
             self.running = False
             self.clock_event.set()
+
 class SubSystem2(Thread):
-    def __init__(self,r1_assigned,r2_assigned): 
+    def __init__(self, resource_requested, queue_weights, r1_assigned, r2_assigned):
         super().__init__()
         self.number_of_cores = 2
         self.r1_assigned = r1_assigned
         self.r2_assigned = r2_assigned
-        self.scheduler = ShortestRemainingJobFirstScheduler(r1_assigned,r2_assigned)
-        self.cores = [SubSystem2Core(self.scheduler, i+1) for i in range(self.number_of_cores)]
+        self.scheduler = ShortestRemainingJobFirstScheduler(r1_assigned, r2_assigned)
+        self.cores = [SubSystem2Core(self.scheduler, i + 1, Queue()) for i in range(self.number_of_cores)]
         self.running = True
         self.lock = threading.Lock()
         self.clock_event = threading.Event()
+        self.resource_manager = ResourceManager(resource_requested)
+        self.queue_weights = queue_weights
+
+        self.add_queues_to_schedulers()
+
+    def add_queues_to_schedulers(self):
+        for core in self.cores:
+            for queue, weight in zip(self.ready_queues, self.queue_weights):
+                core.add_queue(weight)
+
     def start_cores(self):
         for core in self.cores:
             core.start()
-    def stop_cores(self):   
+
+    def stop_cores(self):
         for core in self.cores:
             core.stop()
             core.join()
-    def set_clock_event(self):  
+
+    def set_clock_event(self):
         with self.lock:
             self.clock_event.set()
+
     def run(self):
         self.start_cores()
         while True:
@@ -69,3 +87,8 @@ class SubSystem2(Thread):
                     core.set_clock_event()
                 self.clock_event.clear()
         self.stop_cores()
+
+    def stop(self):
+        with self.lock:
+            self.running = False
+            self.clock_event.set()
--- sub_systems/sub_system_3/sub_system_3.py
+++ sub_systems/sub_system_3/sub_system_3.py
@@ -1,24 +1,27 @@
 import threading
 from queue import Queue
 from threading import Thread
-from core import BaseCore  
+from core import BaseCore
 from rate_monotonic import RateMonotonicScheduler
-from task import BaseTask  
+from task import BaseTask
+from resource_manager import ResourceManager
+
 class SubSystem3Core(BaseCore):
-    def __init__(self, scheduler: RateMonotonicScheduler, core_id):
-        super().__init__(scheduler, core_id, Queue(), None)  
-        self.scheduler = scheduler
-        self.core_id = core_id
-        self._lock = threading.Lock()
+    def __init__(self, scheduler: RateMonotonicScheduler, core_id, ready_queue, waiting_queue):
         self.clock_event = threading.Event()
+        self._lock = threading.Lock()
+        super().__init__(scheduler, core_id, ready_queue, waiting_queue)
 
     def run_task(self, task: BaseTask):
         task_status = task.execute()
         if task_status == 0:
-            print(f"Task {task.name} completed on Core {self.core_id}")
-            self.scheduler.release_resources(task) 
+            print(f"Task {task.name} completed on core {self.core_id}")
+            self.queue_scheduler.release_resources(task)
         elif task_status == -1:
-            print(f"Task {task.name} failed on Core {self.core_id}")
+            print(f"Task {task.name} failed on core {self.core_id}")
+
+    def add_queue(self, weight):
+        self.queue_scheduler.add_queue(self.ready_queue, weight)
 
     def run(self):
         while True:
@@ -26,7 +29,7 @@ class SubSystem3Core(BaseCore):
             with self._lock:
                 if not self.running:
                     break
-                task = self.scheduler.get_next_task()
+                task = self.queue_scheduler.get_next_task()
                 if task:
                     self.run_task(task)
                 self.clock_event.clear()
@@ -41,35 +44,62 @@ class SubSystem3Core(BaseCore):
             self.clock_event.set()
 
 class SubSystem3(Thread):
-    def __init__(self, r1_assigned, r2_assigned):
+    def __init__(self, resource_requested, queue_weights, r1_assigned, r2_assigned):
         super().__init__()
-        self.num_cores = 1 
-        self.r1_assigned = r1_assigned
-        self.r2_assigned = r2_assigned
-        self.scheduler = RateMonotonicScheduler(r1_assigned, r2_assigned)
-        self.core = SubSystem3Core(self.scheduler, 1)
-        self.running = True
         self._lock = threading.Lock()
         self.clock_event = threading.Event()
 
-    def start_core(self):
-        self.core.start()
+        self.num_cores = 1
 
-    def stop_core(self):
-        self.core.stop()
-        self.core.join()
+        self.ready_queues = [Queue() for _ in range(self.num_cores)]
+        self.waiting_queue = Queue()
+
+        self.r1_assigned = r1_assigned
+        self.r2_assigned = r2_assigned
+
+        self.cores = [
+            SubSystem3Core(
+                RateMonotonicScheduler(self.waiting_queue, self.r1_assigned, self.r2_assigned, lock=self._lock),
+                i + 1, self.ready_queues[i], self.waiting_queue) for i in range(self.num_cores)
+        ]
+        self.resource_manager = ResourceManager(resource_requested)
+        self.running = True
+
+        self.queue_weights: list = queue_weights
+
+        self.add_queues_to_schedulers()
+
+    def add_queues_to_schedulers(self):
+        for core in self.cores:
+            for queue, weight in zip(self.ready_queues, self.queue_weights):
+                core.add_queue(weight)
+
+    def stop(self):
+        with self._lock:
+            self.running = False
+            self.clock_event.set()
+
+    def start_cores(self):
+        for core in self.cores:
+            core.start()
+
+    def stop_cores(self):
+        for core in self.cores:
+            core.stop()
+            core.join()
 
     def set_clock_event(self):
         with self._lock:
             self.clock_event.set()
 
     def run(self):
-        self.start_core()
+        self.start_cores()
         while True:
             self.clock_event.wait()
             with self._lock:
                 if not self.running:
                     break
-                self.core.set_clock_event()
+                for core in self.cores:
+                    core.set_clock_event()
                 self.clock_event.clear()
-        self.stop_core()
+        self.stop_cores()
