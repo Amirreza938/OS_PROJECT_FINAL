d3ae91da2f8216e40b2f49fb1662a2b1f53599a0
mhkapr <mhkaprogrammer@gmail.com>
2025-01-21 16:53:15 +0330

    some changes for main system

--- sub_systems/main_system.py
+++ sub_systems/main_system.py
@@ -7,6 +7,7 @@ class MainSystem(Thread):
         self.running = True
         self._lock = Lock()
         self.clock_event = Event()
+        self.finish_flags = [False for _ in range(len(self.sub_systems))]
 
     def toggle_clock(self):
         self.clock_event.set()
@@ -20,11 +21,31 @@ class MainSystem(Thread):
             self.running = False
             self.clock_event.set()
 
+    def run_sub_systems(self):
+        for sub_system in self.sub_systems:
+            sub_system.start()
+
+    def check_finish_time(self):
+        finish_flag = True
+        for flag in self.finish_flags:
+            finish_flag &= flag
+        return finish_flag
+
+    def join_subsystems(self):
+        for sub_system in self.sub_systems:
+            sub_system.join()
+            sub_system.finish()
+
+
     def run(self):
+        self.run_sub_systems()
         while True:
             self.clock_event.wait()
             with self._lock:
                 if not self.running:
                     break
                 self.toggle_sub_systems()
+                if self.check_finish_time():
+                    self.join_subsystems()
+                    break
                 self.clock_event.clear()
