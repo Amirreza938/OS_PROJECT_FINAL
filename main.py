9499b36c359fddd03568c6a0570ff36996657fca
Amirreza938 <120088072+Amirreza938@users.noreply.github.com>
2025-01-24 17:40:30 +0330

    run two subsystems at the same time

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ def main():
     }
     queue_weights = [1, 1]
 
-    #task: SubSystem1Task = SubSystem1Task("T1", 10, 1, 1,3, 0)
-    task: SubSystem2Task = SubSystem2Task("T1",10,1,1,0,2)
+    task: SubSystem1Task = SubSystem1Task("T4", 10, 1, 1,3, 0)
+    task0: SubSystem2Task = SubSystem2Task("T1",10,1,1,0,2)
     task1: SubSystem2Task = SubSystem2Task("T2",10,1,1,1,2)
     task2: SubSystem2Task = SubSystem2Task("T3",10,1,1,2,2)
     resource_1_finish_flag = False
     resource_2_finish_flag = False
-    #sub_system_1 = SubSystem1(total_resources, queue_weights, 4, 4, [task], resource_1_finish_flag)
-    sub_system_2 = SubSystem2(total_resources,4,4,[task,task1,task2],resource_2_finish_flag)
-    main_system = MainSystem([sub_system_2])
+    sub_system_1 = SubSystem1(total_resources, queue_weights, 4, 4, [task], resource_1_finish_flag)
+    sub_system_2 = SubSystem2(total_resources,4,4,[task0,task1,task2],resource_2_finish_flag)
+    main_system = MainSystem([sub_system_1,sub_system_2])
     main_system.start()
 
     for _ in range(100):
